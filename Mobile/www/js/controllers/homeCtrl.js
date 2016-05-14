175e72afafff7a5f6a0d45c6cb243e5dbc31239e
Mohamed Elkady <mohamedelkady@gmail.com>
2016-05-14 19:02:26 +0400

    Bug fixes

--- Mobile/www/js/controllers/homeCtrl.js
+++ Mobile/www/js/controllers/homeCtrl.js
@@ -1,57 +1,65 @@
 angular.module('imageID.controllers').controller('HomeCtrl', function($scope, $timeout, $state, $log, Restangular, MemoryService, $rootScope, $ionicSlideBoxDelegate, $ionicHistory) {
-	$scope.data = {};
-	$scope.data.ads = [];
-	$scope.data.featured = [];
+    $scope.data = {};
+    $scope.data.ads = [];
+    $scope.data.featured = [];
 
-	$ionicHistory.clearHistory();
+    $ionicHistory.clearHistory();
 
-	$scope.adsPromise = Restangular.all('type/ad').getList().then(function(posts) {
-        if(posts.length > 2){
-            $scope.data.ads = [ posts[0], posts[1] ];
+    $scope.adsPromise = Restangular.all('type/ad').getList().then(function(posts) {
+        if (posts.length > 2) {
+            $scope.data.ads = [posts[0], posts[1]];
         } else {
             $scope.data.ads = posts;
         }
-	}).catch(function(err) {
-		$scope.data.ads = [];
-	});
+    }).catch(function(err) {
+        $scope.data.ads = [];
+    });
 
-	$scope.featuredPromise = Restangular.all('homepage').getList().then(function(posts) {
-		$scope.data.featured = posts;
+    $scope.featuredPromise = Restangular.all('homepage').getList().then(function(posts) {
+        $scope.data.featured = posts;
 
         $timeout(function() {
-        	$scope.$apply();
-			$ionicSlideBoxDelegate.update();
-    	}, 100);
-	}).catch(function(err) {
-		$scope.data.featured = [];
-	});
+            $scope.$apply();
+            $ionicSlideBoxDelegate.update();
+        }, 100);
+    }).catch(function(err) {
+        $scope.data.featured = [];
+    });
+
+    $scope.next = function(handle) {
+        $ionicSlideBoxDelegate.$getByHandle(handle).next();
+    }
+
+    $scope.previous = function(handle) {
+        $ionicSlideBoxDelegate.$getByHandle(handle).previous();
+    }
 
 
-	$scope.openDetails = function(v){
-		// Promotable types:
-		// 'designer', 'vendor', 'showroom', 'catalog', 'product', 'house'
-		if(v.post_type == "designer") {
-			MemoryService.putData("designer", v);
-			$state.go("app.designer", { "id" : v.id });
-		} else if(v.post_type == "vendor") {
-			$state.go("app.vendor", { "id" : v.id });
-		} else if (v.post_type == "showroom"){
-			$state.go("app.showroom", { "id" : v.id });
-		} else if (v.post_type == "catalog"){
-			if(ionic.Platform.isAndroid()){
-				window.open("https://docs.google.com/gview?embedded=true&url=" + v.file, '_blank', 'location=no');
-			} else {
-				window.open(v.file, '_blank', 'location=no');
-			}
-		} else if (v.post_type == "house"){
-			MemoryService.putData("house", v);
-			$state.go("app.house", { "id" : v.id });
-		} else if (v.post_type == "product"){
-			$state.go("app.product", { "id" : v.id });
-		}
-	}
+    $scope.openDetails = function(v) {
+        // Promotable types:
+        // 'designer', 'vendor', 'showroom', 'catalog', 'product', 'house'
+        if (v.post_type == "designer") {
+            MemoryService.putData("designer", v);
+            $state.go("app.designer", { "id": v.id });
+        } else if (v.post_type == "vendor") {
+            $state.go("app.vendor", { "id": v.id });
+        } else if (v.post_type == "showroom") {
+            $state.go("app.showroom", { "id": v.id });
+        } else if (v.post_type == "catalog") {
+            if (ionic.Platform.isAndroid()) {
+                window.open("https://docs.google.com/gview?embedded=true&url=" + v.file, '_blank', 'location=no');
+            } else {
+                window.open(v.file, '_blank', 'location=no');
+            }
+        } else if (v.post_type == "house") {
+            MemoryService.putData("house", v);
+            $state.go("app.house", { "id": v.id });
+        } else if (v.post_type == "product") {
+            $state.go("app.product", { "id": v.id });
+        }
+    }
 
-	$scope.openAd = function(ad){
-		window.open(ad.url, "_system");
-	}
+    $scope.openAd = function(ad) {
+        window.open(ad.url, "_system");
+    }
 });
